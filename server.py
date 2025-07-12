03aef3ad0e314af5de068f8789ad81d1abd088c0
Iskar Deng <denghaowen@126.com>
2025-07-12 00:44:59 -0700

    1.1.0 draft add ping

--- server.py
+++ server.py
@@ -31,10 +31,6 @@ def get_games():
         traceback.print_exc()
         return jsonify({"error": str(e)}), 500
 
-@app.route("/ping")
-def ping():
-    return "pong"
-
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
